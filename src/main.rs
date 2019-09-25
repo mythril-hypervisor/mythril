1c20c9a8fc05ba1dfb8379f9bfe25d33cba56fc1
Adam Schwalm <adamschwalm@gmail.com>
2019-09-25 18:48:34 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate log;
+
+extern crate uefi_services;
+
+#[no_mangle]
+pub static _fltused: u32 = 0;
+
+use uefi::prelude::*;
+
+#[entry]
+fn efi_main(handle: Handle, system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&system_table).expect_success("Failed to initialize utilities");
+
+    info!("here");
+    loop {}
+
+    Status::SUCCESS
+}
