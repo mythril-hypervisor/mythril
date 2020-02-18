30fdb8940b68fa07abccc510bdf2212174188f5f
Adam Schwalm <adamschwalm@gmail.com>
2020-02-17 18:30:25 -0600

    Move GDT to mythril_core and factor out paging %defines

--- mythril_core/build.rs
+++ mythril_core/build.rs
@@ -3,6 +3,8 @@ use nasm_rs;
 fn main() {
     nasm_rs::Build::new()
         .file("src/vm.S")
+        .file("src/boot.S")
+        .include("asm_include/")
         .target("x86_64-unknown-none")
         .compile("vm");
     println!("cargo:rustc-link-lib=static=vm");
--- mythril_multiboot2/build.rs
+++ mythril_multiboot2/build.rs
@@ -4,6 +4,7 @@ fn main() {
     nasm_rs::Build::new()
         .file("src/header.S")
         .file("src/boot.S")
+        .include("../mythril_core/asm_include/")
         .target("x86_64-unknown-none")
         .compile("header");
     println!("cargo:rustc-link-lib=static=header");
