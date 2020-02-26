6316881a360d80e741dc4d4583762d78fc07f783
Adam Schwalm <adamschwalm@gmail.com>
2020-02-25 20:13:42 -0600

    Add DMA controller stub

---
+++ mythril_core/src/device/dma.rs
@@ -0,0 +1,48 @@
+use crate::device::{DeviceRegion, EmulatedDevice, Port, PortIoValue};
+use crate::error::Result;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
+#[derive(Default, Debug)]
+pub struct Dma8237;
+
+#[allow(dead_code)]
+impl Dma8237 {
+    const DMA1_CHAN2_ADDR: Port = 0x0004;
+    const DMA1_CHAN2_COUNT: Port = 0x0005;
+    const DMA1_MASK: Port = 0x000a;
+    const DMA1_MODE: Port = 0x000b;
+    const DMA1_CLEAR_FF: Port = 0x000c;
+    const DMA1_MASTER_CLEAR: Port = 0x000d;
+
+    // This order is correct. 2-3-1
+    const DMA_CHAN2_PAGE_CHECK: Port = 0x0081;
+    const DMA_CHAN3_PAGE_CHECK: Port = 0x0082;
+    const DMA_CHAN1_PAGE_CHECK: Port = 0x0083;
+
+    const DMA2_MASK: Port = 0x00d4;
+    const DMA2_MODE: Port = 0x00d6;
+    const DMA2_MASTER_CLEAR: Port = 0x00da;
+
+    pub fn new() -> Box<Self> {
+        Box::new(Dma8237::default())
+    }
+}
+
+impl EmulatedDevice for Dma8237 {
+    fn services(&self) -> Vec<DeviceRegion> {
+        vec![
+            DeviceRegion::PortIo(Self::DMA1_CHAN2_ADDR..=Self::DMA1_MASTER_CLEAR),
+            DeviceRegion::PortIo(Self::DMA_CHAN2_PAGE_CHECK..=Self::DMA_CHAN1_PAGE_CHECK),
+            DeviceRegion::PortIo(Self::DMA2_MASK..=Self::DMA2_MASTER_CLEAR),
+        ]
+    }
+
+    fn on_port_read(&mut self, _port: Port, _val: &mut PortIoValue) -> Result<()> {
+        Ok(())
+    }
+
+    fn on_port_write(&mut self, _port: Port, _val: PortIoValue) -> Result<()> {
+        Ok(())
+    }
+}
