f87c2189efdae8f203a446c4a99964b56c597659
Adam Schwalm <adamschwalm@gmail.com>
2020-04-05 21:40:20 -0500

    Fix logging locks

--- mythril_core/src/logger.rs
+++ mythril_core/src/logger.rs
@@ -6,7 +6,11 @@ static LOG_LOCK: Mutex<()> = Mutex::new(());
 
 pub fn write_console(s: impl AsRef<str>) {
     let _lock = LOG_LOCK.lock();
+    raw_write_console(s)
+}
 
+// NOTE: the caller should hold `LOG_LOCK`
+fn raw_write_console(s: impl AsRef<str>) {
     //FIXME: what about addresses above 4GB?
     let len = s.as_ref().len();
     let ptr = s.as_ref().as_ptr();
@@ -48,7 +52,14 @@ impl log::Log for DirectLogger {
 struct DirectWriter;
 impl fmt::Write for DirectWriter {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        write_console(s);
+        raw_write_console(s);
         Ok(())
+    }
+
+    fn write_fmt(&mut self, args: fmt::Arguments) -> Result<(), fmt::Error> {
+        // Acquire the lock at the formatting stage, so the args will not
+        // race with the guest console (that calls `write_console` directly)
+        let _lock = LOG_LOCK.lock();
+        fmt::write(self, args)
     }
 }
