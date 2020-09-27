d9e26b8d2c88498af7b5fb62007e7dde22d8b48c
Adam Schwalm <adamschwalm@gmail.com>
2020-09-26 21:58:09 -0500

    Add invvpid/invept support

--- mythril/src/vmx.rs
+++ mythril/src/vmx.rs
@@ -76,4 +76,54 @@ impl Vmx {
     pub fn revision() -> u32 {
         unsafe { msr::rdmsr(msr::IA32_VMX_BASIC) as u32 }
     }
+
+    pub fn invept(&self, mode: InvEptMode) -> Result<()> {
+        let (t, val) = match mode {
+            InvEptMode::SingleContext(eptp) => (1u64, eptp as u128),
+            InvEptMode::GlobalContext => (2u64, 0 as u128),
+        };
+
+        let rflags = unsafe {
+            let rflags: u64;
+            llvm_asm!("invept $1, $2; pushfq; popq $0"
+                      : "=r"(rflags)
+                      : "m"(val), "r"(t));
+            rflags
+        };
+        error::check_vm_insruction(rflags, "Failed to execute invept".into())
+    }
+
+    pub fn invvpid(&self, mode: InvVpidMode) -> Result<()> {
+        let (t, val) = match mode {
+            InvVpidMode::IndividualAddress(vpid, addr) => {
+                (0u64, vpid as u128 | ((addr.as_u64() as u128) << 64))
+            }
+            InvVpidMode::SingleContext(vpid) => (1u64, vpid as u128),
+            InvVpidMode::AllContext => (2u64, 0u128),
+            InvVpidMode::SingleContextRetainGlobal(vpid) => {
+                (3u64, vpid as u128)
+            }
+        };
+
+        let rflags = unsafe {
+            let rflags: u64;
+            llvm_asm!("invvpid $1, $2; pushfq; popq $0"
+                      : "=r"(rflags)
+                      : "m"(val), "r"(t));
+            rflags
+        };
+        error::check_vm_insruction(rflags, "Failed to execute invvpid".into())
+    }
+}
+
+pub enum InvEptMode {
+    SingleContext(u64),
+    GlobalContext,
+}
+
+pub enum InvVpidMode {
+    IndividualAddress(u16, GuestVirtAddr),
+    SingleContext(u16),
+    AllContext,
+    SingleContextRetainGlobal(u16),
 }
