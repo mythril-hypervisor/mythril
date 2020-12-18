c4a6ff222b2edab625abd714ad087668a2369a8f
Francis Nixon <francis.nixon@starlab.io>
2020-12-18 13:08:39 -0500

    got some basic cpuid stuff working.

--- mythril/src/emulate/cpuid.rs
+++ mythril/src/emulate/cpuid.rs
@@ -1,18 +1,81 @@
 use crate::error::Result;
 use crate::{vcpu, vmexit};
+use raw_cpuid::CpuIdResult;
+use arrayvec::ArrayVec;
+use core::convert::TryInto;
+use bitfield::bitfield;
 
 //Used https://c9x.me/x86/html/file_module_x86_id_45.html as guid for implementing this.
 const CPUID_NAME: u32 = 0;
 const CPUID_MODEL_FAMILY_STEPPING: u32 = 1;
 const CPUID_CACHE_TLB_INFO: u32 = 2;
-const INTEL_CORE_CACHE_TOPOLOGY : u32 = 4;
+const INTEL_CORE_CACHE_TOPOLOGY: u32 = 4;
 const CPUID_BRAND_STRING_1: u32 = 0x80000002;
 const CPUID_BRAND_STRING_2: u32 = 0x80000003;
 const CPUID_BRAND_STRING_3: u32 = 0x80000004;
+const MAX_CPUID_INPUT: u32 = 0x80000004;
 //todo //CPUID leaves above 2 and below 80000000H are visible only when
 //     // IA32_MISC_ENABLE[bit 22] has its default value of 0.
 
 
+//
+// bitfield! {
+//     pub struct IntelCoreCacheTopologyEaxRes(u32)
+//     impl Debug;
+//     impl Copy;
+//
+// }
+
+bitfield! {
+    pub struct IntelTypeFamilyModelSteppingIDEaxRes(u32);
+    impl Debug;
+    stepping_id, _: 3,0;
+    model,_:7,4;
+    family_id,_:11,8;
+    processor_type,_:13,12;
+    extended_model_id,_:19,16;
+    extended_family_id,_:27,20;
+}
+
+fn get_cpu_id_result(vcpu: &vcpu::VCpu, eax: u32, ecx: u32) -> Option<CpuIdResult> {
+    const NAME_CREATION_ERROR_MESSAGE: &'static str = "Somehow bytes was not actually a 12 element array";
+
+    match eax {
+        CPUID_NAME => {
+            if vcpu.vm.read().config.override_cpu_name() {
+                let cpu_name = "MythrilCPU__";
+                let bytes = cpu_name.chars().map(|char| char as u8).collect::<ArrayVec<[u8; 12]>>();
+                let first_bytes: [u8; 4] = bytes[0..4].try_into().expect(NAME_CREATION_ERROR_MESSAGE);
+                let second_bytes: [u8; 4] = bytes[4..8].try_into().expect(NAME_CREATION_ERROR_MESSAGE);
+                let third_bytes: [u8; 4] = bytes[8..12].try_into().expect(NAME_CREATION_ERROR_MESSAGE);
+                return Some(CpuIdResult {
+                    eax: MAX_CPUID_INPUT,
+                    ebx: u32::from_le_bytes(first_bytes),
+                    ecx: u32::from_le_bytes(second_bytes),
+                    edx: u32::from_le_bytes(third_bytes),
+                });
+            }
+        }
+        CPUID_MODEL_FAMILY_STEPPING => {
+
+        }
+        INTEL_CORE_CACHE_TOPOLOGY => {
+            let core_cpus = vcpu.vm.read().config.cpus();
+
+            todo!()
+        }
+        CPUID_BRAND_STRING_1..=CPUID_BRAND_STRING_2 => {
+            if vcpu.vm.read().config.override_cpu_name() { todo!("CPU Brand string not implemented yet") }
+            return None;
+        }
+        _ => {
+            //TODO for code review. Idk how I feel about silently fallingback on real cpuid here.
+            // I would perhaps prefer to put a todo!() and explicitly implement stuff.
+            return None;
+        }
+    };
+    panic!()
+}
 
 pub fn emulate_cpuid(
     vcpu: &mut vcpu::VCpu,
@@ -20,23 +83,6 @@ pub fn emulate_cpuid(
 ) -> Result<()> {
     let eax = guest_cpu.rax as u32;
 
-    match eax {
-        CPUID_NAME => {
-            if vcpu.vm.read().config.override_cpu_name(){
-                todo!()
-            }
-        },
-        CPUID_MODEL_FAMILY_STEPPING => todo!(),
-        INTEL_CORE_CACHE_TOPOLOGY => {
-            _vcpu.vm.read().config.cpus()
-        }
-        CPUID_BRAND_STRING_1 => todo!(),
-        CPUID_BRAND_STRING_2 => todo!(),
-        _ => {
-            // dbg!(eax);
-            // todo!("If you are reading this then a invalid arg was passed to cpuid. In principle we should prob fault here or something, but this probably indicates a bug.")
-        }
-    }
 
     //FIXME: for now just use the actual cpuid
     let mut res = raw_cpuid::native_cpuid::cpuid_count(
